8a628556f62cc3acb6eb94ef29120198eb4becdb
André Felipe Siqueira <andrefs.995@gmail.com>
2024-07-31 12:45:40 +0100

    Animation Rigging alpha

---
+++ Scripts/SetMultiAimTarget.cs
@@ -0,0 +1,24 @@
+﻿using Z3.NodeGraph.Tasks;
+using UnityEngine;
+using Z3.NodeGraph.Core;
+using UnityEngine.Animations.Rigging;
+
+namespace Z3.NodeGraph.TaskPack.AnimationRigging
+{
+    [NodeCategory(Categories.AnimationRigging)]
+    [NodeDescription("TODO")]
+    public class SetMultiAimTarget : ActionTask<MultiAimConstraint> // AimIK
+    {
+        public Parameter<Transform> target;
+
+        public override string Info => $"{base.Info} = {target}";
+
+        protected override void StartAction()
+        {
+            //IKSolverAim ikAim = agent.GetIKSolver() as IKSolverAim;
+
+            //ikAim.target = target.value;
+            EndAction();
+        }
+    }
+}
---
+++ Scripts/SetRigConstraintWeight.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using Z3.NodeGraph.Tasks;
+using Z3.NodeGraph.Core;
+
+namespace Z3.NodeGraph.TaskPack.AnimationRigging
+{
+    [NodeCategory(Categories.AnimationRigging)]
+    [NodeDescription("TODO")]
+    public class SetRigConstraintWeight : ActionTask
+    {
+        public Parameter<IRigConstraint> rigConstraint;
+
+        public bool useSpeed = true;
+        //[MinMaxSlider(0f, 1f)]
+        public Parameter<float> weight;
+        //[ShowIf(nameof(useSpeed), 1)]
+        public Parameter<float> duration;
+
+        private float currentWeight;
+
+        public override string Info => $"Set {rigConstraint} Weight = {weight}";
+
+        protected override void StartAction()
+        {
+
+            if (useSpeed)
+            {
+                currentWeight = rigConstraint.Value.weight;
+            }
+            else
+            {
+                rigConstraint.Value.weight = weight.Value;
+                EndAction();
+            }
+        }
+
+        protected override void UpdateAction()
+        {
+            currentWeight = Mathf.MoveTowards(currentWeight, weight.Value, 1f / duration.Value * Time.fixedDeltaTime);
+            rigConstraint.Value.weight = currentWeight;
+
+            if (currentWeight == weight.Value)
+            {
+                EndAction();
+            }
+        }
+    }
+}
